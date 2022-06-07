2148af53bf17442e86633b478733552d72f07be4
OmriKellnerMoveo <omrik@moveo.co.il>
2022-06-07 09:59:55 +0300

    fix tags

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,20 +1,20 @@
-import {Injectable} from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class StorageService<T> {
-
-  constructor() {
-  }
-
-  save(key: string, value: T) {
-    localStorage.setItem(key, JSON.stringify(value))
-    return value
-  }
-
-  query(key: string): T {
-    const value = localStorage.getItem(key)
-    return value ? JSON.parse(value) : null
-  }
-}
+// import {Injectable} from '@angular/core';
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class StorageService<T> {
+//
+//   constructor() {
+//   }
+//
+//   save(key: string, value: T) {
+//     localStorage.setItem(key, JSON.stringify(value))
+//     return value
+//   }
+//
+//   query(key: string): T {
+//     const value = localStorage.getItem(key)
+//     return value ? JSON.parse(value) : null
+//   }
+// }
